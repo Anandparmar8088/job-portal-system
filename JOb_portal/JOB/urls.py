be5cf5793511e273d01de1fcf6eee36a1fc6f3fa
Anandparmar8088 <parmaranand031@gmail.com>
2025-08-28 01:28:55 +0530

    frontend for home page os doen

--- JOb_portal/JOB/urls.py
+++ JOb_portal/JOB/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from .import views
 
 urlpatterns = [
-    path('dt/', views.Job_posting),
-    path('dt/', views.Job_see),
+    path('job/', views.Job_posting),
+    # path('dt/', views.Job_see),
     
 ]
 #   have to add new urls
